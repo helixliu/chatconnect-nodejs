264bbcdc2611f98eca94f5fc928dd148d0c83eb3
helixliu <34231528+helixliu@users.noreply.github.com>
2020-05-19 14:15:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //  OpenShift sample Node application
-var wechatconfig = require('./config/wechatconfig.json'),
+var config = require('./config/config.json'),
     express = require('express'),
     app     = express(),
     wechatutils  = require('./common/wechatutils');
@@ -7,7 +7,7 @@ var wechatconfig = require('./config/wechatconfig.json'),
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.use('/wechatconnect',wechatutils.sign(wechatconfig));
+app.use('/wechatconnect',wechatutils.sign(config));
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
